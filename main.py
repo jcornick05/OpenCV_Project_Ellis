1f1c36aed4a5693e3b7e4f5d5616a8226f35e0e1
jcornick05 <jscornick@gmail.com>
2022-09-12 10:35:38 -0700

    Added trackbar
    Added a very basic track bar

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import argparse
 import cv2
 
+def on_change(val):
+    print(val)
+    
+    imageCopy = image.copy()
+
+    cv2.putText(imageCopy, str(val), (0, imageCopy.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 4)
+    cv2.imshow(winName, imageCopy)
+
+# Gets the image from the user
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Path to the image")
 args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-cv2.imshow("Original", image)
+image = cv2.imread(args["image"]) # The image the user would like to use
+
+winName = "Default"
+
+cv2.imshow(winName, image)
+cv2.createTrackbar('slider', winName, 0, 255, on_change) # Creates the trackbar
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
