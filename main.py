36d6cd2a331acdb2ed2d3a380648fec1f2dd3b4b
jcornick05 <jscornick@gmail.com>
2022-09-21 23:24:05 -0400

    Color Histogram
    Added the option to display a color histogram

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # Imports
+from matplotlib import pyplot as plt
 import argparse
 import cv2
 import imutils
-import os
+import numpy as np
+
+def displayHistogram(image):
+    chans = cv2.split(image)
+    color = "b", "g", "r"
+    plt.figure()
+    plt.title("Color Histogram")
+    plt.xlabel("Bins")
+    plt.ylabel("# of Pixels")
+    for (chan, color) in zip(chans, color):
+        hist = cv2.calcHist([chan], [0], None, [256], [0, 256])
+        plt.plot(hist, color = color)
+        plt.xlim([0, 256])
+
+    fig = plt.figure
+    hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    plt.show()
 
 #-----------------------------------------------------------------#
+
 def on_change(val):
     #print(val)
     imageCopy = image.copy()
@@ -30,6 +48,22 @@ def on_change_move(val):
 
 #-----------------------------------------------------------------#
 
+def on_change_arithmetic(val):
+    type = int(cv2.getTrackbarPos("type", winName))
+    change_val = int(cv2.getTrackbarPos("change", winName))
+
+    global updated
+
+    if type == 0:
+        M = np.ones(image.shape, dtype = "uint8") * change_val
+        updated = cv2.add(image, M)
+    else:
+        M = np.ones(image.shape, dtype = "uint8") * change_val
+        updated = cv2.subtract(image, M)
+
+    cv2.imshow(winName, updated)
+
+#-----------------------------------------------------------------#
 
 # Gets the image from the user
 ap = argparse.ArgumentParser()
@@ -39,13 +73,14 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"]) # The image the user would like to use
 winName = "Default" # Name of the window
 
-editPhrase = ["tester", "move"]
+editPhrase = ["tester", "move", "arithmetic"]
 EDITED = False
 CONTINUE = True
+DISPLAY_HIST = False
 while CONTINUE:
     cv2.imshow(winName, image)
     print("What would you like to do?")
-    option = str(input(" --> "))
+    option = str(input(" --> ")).lower()
 
     if option in editPhrase:
         if option == "tester":
@@ -56,11 +91,19 @@ while CONTINUE:
             cv2.setTrackbarMin('x', winName, -100) # Redefines trackbar bounds
             cv2.createTrackbar('y', winName, 0, 100, on_change_move) # Creates the trackbar
             cv2.setTrackbarMin('y', winName, -100) # Redefines trackbar bounds
+        elif option == "arithmetic":
+            cv2.createTrackbar('type', winName, 0, 1, on_change_arithmetic) # Creates the trackbar
+            cv2.createTrackbar('change', winName, 0, 255, on_change_arithmetic) # Creates the trackbar
             
         cv2.waitKey(0)
-        image = updated # Redefines the updated image
+        image = updated
+        # Redefines the updated image
         cv2.destroyAllWindows() 
         EDITED = True # Allows the user to save edits
+
+    elif option == "show histogram":
+        displayHistogram(image)
+        DISPLAY_HIST = True
 
     elif option == "save":
         if EDITED:
